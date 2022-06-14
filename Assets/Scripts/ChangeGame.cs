a0b9a7d7fc54de583b7b8412fe0441533791efbd
mynhung01 <nhungldmse151295@fpt.edu.vn>
2022-06-14 17:20:48 +0700

    SlotMachine Game

--- Assets/Scripts/ChangeGame.cs
+++ Assets/Scripts/ChangeGame.cs
@@ -9,4 +9,8 @@ public class ChangeGame : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void SLotMachine()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 }
